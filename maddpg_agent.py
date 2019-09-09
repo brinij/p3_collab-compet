1162ccfcc4aa8fa3ab9ccb5704cd674657cbdb4e
brinij <inka.brijacak@gmail.com>
2019-09-09 23:07:23 +0200

    Added weights files and network architecture schemes

--- maddpg_agent.py
+++ maddpg_agent.py
@@ -13,7 +13,7 @@ BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 2e-1              # for soft update of target parameters
-LR_ACTOR = 2e-4         # learning rate of the actor 
+LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 3e-4        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
